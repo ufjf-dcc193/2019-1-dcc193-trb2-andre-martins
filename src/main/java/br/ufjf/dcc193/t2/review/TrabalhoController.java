c36407b78f8a0a9a3bc664eeeb75c7988e592081
André F. Martins <martins.adw@gmail.com>
2019-06-16 15:55:36 -0300

    Adicionar remoção de trabalho

--- src/main/java/br/ufjf/dcc193/t2/review/TrabalhoController.java
+++ src/main/java/br/ufjf/dcc193/t2/review/TrabalhoController.java
@@ -5,7 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 // import org.springframework.web.bind.annotation.GetMapping;
-// import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PathVariable;
 // import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -67,10 +67,10 @@ public class TrabalhoController {
     //     return "redirect:/trabalho/{id}";
     // }
 
-    // @RequestMapping("/{id}/delete")
-    // public String delete(@PathVariable Long id) {
-	// 	trabalhoRepo.deleteById(id);
+    @RequestMapping("/{id}/delete")
+    public String delete(@PathVariable Long id) {
+		trabalhoRepo.deleteById(id);
 
-    //     return "redirect:/trabalho";
-    // }
+        return "redirect:/trabalho";
+    }
 }
